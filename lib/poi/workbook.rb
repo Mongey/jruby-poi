3b59325c3a4d934121d48b8c97faf47d56c5f5c0
Conor Mongey <conor@mongey.net>
2022-05-05 16:48:21 +0100

    Update to support jRuby 9.3.4.0

--- lib/poi/workbook.rb
+++ lib/poi/workbook.rb
@@ -5,7 +5,8 @@ module POI
   def self.Facade(delegate, java_class)
     cls = Class.new
     java_class.java_class.java_instance_methods.select{|e| e.public?}.each do | method |
-      args = method.arity.times.collect{|i| "arg#{i}"}.join(", ")
+      arity = method.respond_to?(:parameter_count) ? method.parameter_count : method.arity
+      args = arity.times.collect{|i| "arg#{i}"}.join(", ")
       method_name = method.name.gsub(/([A-Z])/){|e| "_#{e.downcase}"}
       code = "def #{method_name}(#{args}); #{delegate}.#{method.name}(#{args}); end"
 
@@ -15,7 +16,7 @@ module POI
         if method.return_type.to_s == 'boolean'
           code += "\nalias :#{alias_method_name}? :#{method_name}"
         end
-      elsif method_name =~ /^set_[a-z]/ && method.arity == 1
+      elsif method_name =~ /^set_[a-z]/ && arity == 1
         alias_method_name = "#{method_name.sub('set_', '')}"
         code += "\nalias :#{alias_method_name}= :#{method_name}"
         if method.argument_types.first.to_s == 'boolean'
